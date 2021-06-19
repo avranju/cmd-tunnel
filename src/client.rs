de82763a270f06ad13f286ff0b937ddc9096ffb0
Rajasekharan Vengalil <rajave@microsoft.com>
2021-06-19 22:02:34 +0530

    Add base app template

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,31 @@
-fn main() {
-    
+pub mod cmdtunnel {
+    tonic::include_proto!("cmdtunnel");
+}
+
+use cmdtunnel::command_tunnel_client::CommandTunnelClient;
+use cmdtunnel::{command_reply::Output, CommandRequest};
+
+use tonic::Request;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "http://[::1]:7786";
+    let mut client = CommandTunnelClient::connect(addr).await?;
+    let mut stream = client
+        .run(Request::new(CommandRequest {
+            command: "ctest -C Debug -R bsi_local_ut".into(),
+        }))
+        .await?
+        .into_inner();
+
+    while let Some(resp) = stream.message().await? {
+        if let Some(output) = resp.output {
+            match output {
+                Output::Stdout(s) => println!("S: {}", s),
+                Output::Stderr(s) => println!("E: {}", s),
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,51 @@
-fn main() {
-    
+pub mod cmdtunnel {
+    tonic::include_proto!("cmdtunnel");
+}
+
+use cmdtunnel::{
+    command_reply::Output,
+    command_tunnel_server::{CommandTunnel, CommandTunnelServer},
+    CommandReply, CommandRequest,
+};
+use tokio::{
+    sync::mpsc,
+    time::{sleep, Duration},
+};
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{transport::Server, Request, Response, Status};
+
+#[derive(Debug)]
+struct CommandTunnelService;
+
+#[tonic::async_trait]
+impl CommandTunnel for CommandTunnelService {
+    type RunStream = ReceiverStream<Result<CommandReply, Status>>;
+
+    async fn run(&self, req: Request<CommandRequest>) -> Result<Response<Self::RunStream>, Status> {
+        println!("Run command: {}", req.get_ref().command);
+
+        let (tx, rx) = mpsc::channel(100);
+        tokio::spawn(async move {
+            for i in 0..10 {
+                sleep(Duration::from_millis(500)).await;
+                tx.send(Ok(CommandReply {
+                    output: Some(Output::Stdout(format!("{} boo", i + 1).into())),
+                }))
+                .await
+                .unwrap();
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:7786".parse().unwrap();
+    let svr = CommandTunnelServer::new(CommandTunnelService);
+
+    Server::builder().add_service(svr).serve(addr).await?;
+
+    Ok(())
 }
