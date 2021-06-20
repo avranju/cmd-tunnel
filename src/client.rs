a32746003391e4e3c86343c2222d92aa2acdfaa9
Rajasekharan Vengalil <rajave@microsoft.com>
2021-06-20 11:47:41 +0530

    Add main app functionality

--- src/client.rs
+++ src/client.rs
@@ -2,27 +2,31 @@ pub mod cmdtunnel {
     tonic::include_proto!("cmdtunnel");
 }
 
+use std::env;
+
 use cmdtunnel::command_tunnel_client::CommandTunnelClient;
 use cmdtunnel::{command_reply::Output, CommandRequest};
 
+use console::style;
 use tonic::Request;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let command = env::args().nth(1).unwrap_or_default();
+    let args = env::args().skip(2).collect::<Vec<_>>();
+
     let addr = "http://[::1]:7786";
     let mut client = CommandTunnelClient::connect(addr).await?;
     let mut stream = client
-        .run(Request::new(CommandRequest {
-            command: "ctest -C Debug -R bsi_local_ut".into(),
-        }))
+        .run(Request::new(CommandRequest { command, args }))
         .await?
         .into_inner();
 
     while let Some(resp) = stream.message().await? {
         if let Some(output) = resp.output {
             match output {
-                Output::Stdout(s) => println!("S: {}", s),
-                Output::Stderr(s) => println!("E: {}", s),
+                Output::Stdout(s) => println!("{}", style(s).for_stdout()),
+                Output::Stderr(s) => println!("{}", style(s).red()),
             }
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::process::Stdio;
+
 pub mod cmdtunnel {
     tonic::include_proto!("cmdtunnel");
 }
@@ -8,8 +10,9 @@ use cmdtunnel::{
     CommandReply, CommandRequest,
 };
 use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    process::Command,
     sync::mpsc,
-    time::{sleep, Duration},
 };
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
@@ -22,19 +25,48 @@ impl CommandTunnel for CommandTunnelService {
     type RunStream = ReceiverStream<Result<CommandReply, Status>>;
 
     async fn run(&self, req: Request<CommandRequest>) -> Result<Response<Self::RunStream>, Status> {
-        println!("Run command: {}", req.get_ref().command);
+        println!(
+            "Running command: {}, args: {:?}",
+            req.get_ref().command,
+            req.get_ref().args
+        );
 
-        let (tx, rx) = mpsc::channel(100);
-        tokio::spawn(async move {
-            for i in 0..10 {
-                sleep(Duration::from_millis(500)).await;
-                tx.send(Ok(CommandReply {
-                    output: Some(Output::Stdout(format!("{} boo", i + 1).into())),
-                }))
-                .await
-                .unwrap();
-            }
-        });
+        let mut cmd = Command::new(req.get_ref().command.clone())
+            .args(&req.get_ref().args)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+        let stdout = cmd.stdout.take().map(|stdout| BufReader::new(stdout));
+        let stderr = cmd.stderr.take().map(|stderr| BufReader::new(stderr));
+        let stdout_lines = stdout.map(|s| s.lines());
+        let stderr_lines = stderr.map(|s| s.lines());
+        let (tx, rx) = mpsc::channel(1024);
+
+        if let Some(mut stdout_lines) = stdout_lines {
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                while let Some(line) = stdout_lines.next_line().await.unwrap() {
+                    tx.send(Ok(CommandReply {
+                        output: Some(Output::Stdout(line)),
+                    }))
+                    .await
+                    .unwrap();
+                }
+            });
+        }
+
+        if let Some(mut stderr_lines) = stderr_lines {
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                while let Some(line) = stderr_lines.next_line().await.unwrap() {
+                    tx.send(Ok(CommandReply {
+                        output: Some(Output::Stderr(line)),
+                    }))
+                    .await
+                    .unwrap();
+                }
+            });
+        }
 
         Ok(Response::new(ReceiverStream::new(rx)))
     }
@@ -45,6 +77,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:7786".parse().unwrap();
     let svr = CommandTunnelServer::new(CommandTunnelService);
 
+    println!("Listening for commands at http://localhost:7786");
     Server::builder().add_service(svr).serve(addr).await?;
 
     Ok(())
